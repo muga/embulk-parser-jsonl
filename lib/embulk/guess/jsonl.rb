9831d51c3fa1c14c28eb9ffdb851e4d3537fca73
Muga Nishizawa <muga.nishizawa@gmail.com>
2016-02-16 00:11:31 -0800

    Fix guess/jsonl.rb: use GuessPlugin instead of LineGuessPlugin.

--- lib/embulk/guess/jsonl.rb
+++ lib/embulk/guess/jsonl.rb
@@ -5,30 +5,47 @@ module Embulk
   module Guess
     # $ embulk guess -g "jsonl" partial-config.yml
 
-    class Jsonl < LineGuessPlugin # TODO should use GuessPlugin instead of LineGuessPlugin
+    class Jsonl < GuessPlugin
       Plugin.register_guess("jsonl", self)
 
-      def guess_lines(config, sample_lines)
-        #return {} unless config.fetch("parser", {}).fetch("type", "jsonl") == "jsonl"
+      def guess(config, sample_buffer)
+        return {} unless config.fetch("parser", {}).fetch("type", "jsonl") == "jsonl"
 
         rows = []
 
-        columns = {}
-        sample_lines.each do |line|
-          rows << JSON.parse(line)
+        json_parser = new_json_parser(sample_buffer)
+        begin
+          while true
+            v = json_parser.next
+            break unless v
+            rows << JSON.parse(v.toJson)
+          end
+        rescue org.embulk.spi.json::JsonParseException
+          # ignore
         end
 
         return {} if rows.size <= 3
 
-        columns = Embulk::Guess::SchemaGuess.from_hash_records(rows).map do |c|
-          column = {name: c.name, type: c.type}
-          column[:format] = c.format if c.format
-          column
-        end
+        columns = Embulk::Guess::SchemaGuess.from_hash_records(rows)
         parser_guessed = {"type" => "jsonl"}
         parser_guessed["columns"] = columns
+
         return {"parser" => parser_guessed}
       end
+
+      def new_json_parser(buffer)
+        input_stream = java.io::ByteArrayInputStream.new(buffer.to_java_bytes)
+        input_streams = com.google.common.collect::Lists::newArrayList(input_stream)
+
+        buffer_allocator = org.embulk.spi.Exec::getBufferAllocator()
+        iterator_provider = org.embulk.spi.util::InputStreamFileInput::IteratorProvider.new(input_streams)
+        file_input = org.embulk.spi.util::InputStreamFileInput.new(buffer_allocator, iterator_provider)
+
+        input = org.embulk.spi.util::FileInputInputStream.new(file_input)
+        input.nextFile
+        org.embulk.spi.json::JsonParser.new().open(input)
+      end
+
     end
   end
 end
